4acedc90e1f5d510ca38b59bc842e1e6015db40b
Jboy Abecia <jufel.abecia24@gmail.com>
2023-01-22 14:44:14 +0800

    feat: adding page routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import LoadingPage from './pages/LoadingPage'
+import NotFoundPage from './pages/NotFoundPage'
+
+const Homepage = lazy(() => import('./pages/Homepage'))
+const WeatherPage = lazy(() => import('./pages/WeatherPage'))
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+        <Suspense fallback={<LoadingPage />}>
+            <Routes>
+                <Route exact path='/' element={<Homepage />}></Route>
+                <Route path='/weather' element={<WeatherPage />}></Route>
+                <Route path='*' element={<NotFoundPage />}></Route>
+            </Routes>
+        </Suspense>
+    </Router>
   )
 }
 
