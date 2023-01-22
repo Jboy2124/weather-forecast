45332305a5d13712fd6a35854ca436c0f9647edb
Jboy Abecia <jufel.abecia24@gmail.com>
2023-01-22 15:52:55 +0800

    feat: adding landing pages button and protected routes

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -7,8 +7,11 @@ const LandingPage = () => {
         <Navbar />
         <div className='container mx-auto'>
             <div className='min-h-[90vh] flex justify-center items-center'>
-                <div>
-                    <h4>Welcome to the weather forecast web application. Please login with your Github User to use the application and view weather in your city.</h4>
+                <div className='flex flex-col'>
+                    <h4 className='text-[20px]'>Welcome to the weather forecast web application. Please login with your Github User to use the application and view weather in your city.</h4>
+                    <div className='my-4'>
+                        <button type='button' className='rounded-lg shadow-lg bg-[#1F3C88] px-12 py-2 text-[16px] text-white'>Login</button>
+                    </div>
                 </div>
             </div>
         </div>
