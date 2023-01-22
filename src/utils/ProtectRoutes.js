60ef543052de2f7c93fa46adacfb9b7d093d9a45
Jboy Abecia <jufel.abecia24@gmail.com>
2023-01-23 01:45:58 +0800

    refactor: adding profile info (Name & github link )

--- src/utils/ProtectRoutes.js
+++ src/utils/ProtectRoutes.js
@@ -1,13 +1,28 @@
-import { Outlet, Navigate} from "react-router-dom";
-import React from "react";
+// import { Outlet, Navigate} from "react-router-dom";
+// import React from "react";
+// import { useAuth0 } from "@auth0/auth0-react";
+
+// const ProtectRoutes = () => {
+//   const { isAuthenticated } = useAuth0();
+//     return (
+//       (isAuthenticated) ? <Outlet /> : <Navigate to='/login' />
+//     )
+// }
+
+// export default ProtectRoutes
+
+import React from 'react'
+import { Outlet, Navigate } from 'react-router-dom'
 import { useAuth0 } from "@auth0/auth0-react";
 
-const ProtectRoutes = () => {
+const ProtectedRoutes = () => {
   const { isAuthenticated } = useAuth0();
-    return (
-      (isAuthenticated) ? <Outlet /> : <Navigate to='/login' />
-    )
+  let auth = { token: isAuthenticated }
+  console.log(auth)
+
+  return (
+    auth.token ? <Outlet /> : <Navigate to='/login' />
+  )
 }
 
-export default ProtectRoutes
-
+export default ProtectedRoutes
