1720425e684f4337b72e78874f781e5f699388a5
Jboy Abecia <jufel.abecia24@gmail.com>
2023-01-23 02:07:03 +0800

    refactor: weather page code

--- src/pages/WeatherPage.js
+++ src/pages/WeatherPage.js
@@ -5,15 +5,7 @@ import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 const WeatherPage = () => {
-  // const [weatherData, setWeatherData] = useState([])
-  const [city, setCity] = useState('')
-  const [country, setCountry] = useState('')
-  const [dt, setDate] = useState('')
-  const [temp, setTemp] = useState('')
-  const [desc, setDesc] = useState('')
-  const [main, setMain] = useState('')
-  const [pressure, setPressure] = useState('')
-  const [humid, setHumid] = useState('')
+  const [weatherData, setWeatherData] = useState([])
   const currentCity = useSelector((state) => state.currentCity.value)
   const navigate = useNavigate()
 
@@ -25,15 +17,7 @@ const WeatherPage = () => {
         params: { q: currentCity.city },
       })
       .then(response => {
-        setCity(response.data.name)
-        setCountry(response.data.sys.country)
-        setDate(response.data.dt)
-        setTemp(response.data.main.temp)
-        setDesc(response.data.weather[0].description)
-        setMain(response.data.weather[0].main)
-        setPressure(response.data.main.pressure)
-        setHumid(response.data.main.humidity)
-        // setWeatherData(response.data)
+        setWeatherData(response.data)
       })
       .catch(error => {
         console.log(error)
@@ -50,7 +34,7 @@ const WeatherPage = () => {
         <div className='container mx-auto'>
             <div className='min-h-[90vh]'>
 
-                <div>{city}, {country}</div>
+                <div>{weatherData?.name}, {weatherData.sys?.country}</div>
                 <div className='flex justify-center items-center min-h-[30vh] overflow-hidden'>
                    <table className='min-w-full text-center border'>
                       <thead className='border-b bg-gray-500 text-white'>
@@ -65,12 +49,12 @@ const WeatherPage = () => {
                       </thead>
                       <tbody>
                         <tr>
-                          <td className='border-r'>{dt}</td>
-                          <td className='border-r'>{temp}</td>
-                          <td className='border-r'>{desc}</td>
-                          <td className='border-r'>{main}</td>
-                          <td className='border-r'>{pressure}</td>
-                          <td className='border-r'>{humid}</td>
+                          <td className='border-r'>{weatherData?.dt}</td>
+                          <td className='border-r'>{weatherData.main?.temp}</td>
+                          <td className='border-r'>{weatherData.weather?.[0].description}</td>
+                          <td className='border-r'>{weatherData.weather?.[0].main}</td>
+                          <td className='border-r'>{weatherData.main?.pressure}</td>
+                          <td className='border-r'>{weatherData.main?.humidity}</td>
                         </tr>
                       </tbody>
                    </table>
