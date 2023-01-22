0c674d00e14dab8a863bae30186320b89419f98f
Jboy Abecia <jufel.abecia24@gmail.com>
2023-01-22 17:01:43 +0800

    feat: alter in homepage, creating search and adding react-hook-form

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,21 +1,31 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import Searchbar from '../components/Searchbar'
+import { useForm } from 'react-hook-form'
 
 const Homepage = () => {
+  const { register, handleSubmit, formState: { error} } = useForm();
+  const onSubmit = data => console.log(data)
+
   return (
     <div className='bg-white'>
         <Navbar />
         <div className='container mx-auto'>
             <div className='min-h-[90vh] flex justify-center items-center'>
-                <div className='flex flex-col items-center'>
-                    <div className='pb-4'>Name</div>
-                    <div className='pb-4'>github</div>
-                    <Searchbar />
-                    <div className='py-2'>
-                        <button type='submit' className='px-12 py-2 rounded-lg shadow-lg bg-[#1F3C88] text-white'>Display Weather</button>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <div className='flex flex-col items-center'>
+                      <div>Name</div>
+                      <div>github</div>
+                  </div>
+                  <div className='flex items-center w-[400px] h-full rounded-full border border-gray-300 pr-[13px]'>
+                    <div className='flex justify-center items-center text-[22px] text-gray-500 pl-2'>
+                      <ion-icon name="search-outline"></ion-icon>
                     </div>
-                </div>
+                    <input type='text' {...register('city')} placeholder='City' className='text-[14px] outline-none h-[35px] w-full px-2'></input>
+                  </div>
+                  <div className='flex justify-center items-center py-3'>
+                    <button type='submit' className='rounded-lg shadow-lg bg-[#1F3C88] px-12 py-2 text-white'>Display Weather</button>
+                  </div>
+                </form>
             </div>
         </div>
     </div>
--- src/pages/LoadingPage.js
+++ src/pages/LoadingPage.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import PuffLoader from 'react-spinners/PuffLoader'
 
 const LoadingPage = () => {
   return (
-    <div>LoadingPage</div>
+    <div className='bg-white'>
+        <div className='contaier mx-auto'>
+            <div className={`flex flex-col justify-center items-center min-h-screen`}>
+                <PuffLoader 
+                  size={100}
+                  color='#1F3C88'
+                  loading={true}
+                  speedMultiplier={2}
+                />
+                <span className='text-[16px] text-primary font-poppins'>Loading Page...</span>
+            </div>
+        </div>
+    </div>
   )
 }
 
