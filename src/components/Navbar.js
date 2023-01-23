9bc3d293851e282c3d3061bc8508417c30b4edeb
Jboy Abecia <jufel.abecia24@gmail.com>
2023-01-23 11:13:38 +0800

    refactor: creating constant value for the nav menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,47 @@
 import React from 'react'
 import LogoutButton from './LogoutButton'
+// import { menu } from '../constants/menu-const'
+// import { Link } from 'react-router-dom'
+// import { useAuth0 } from '@auth0/auth0-react';
 
 const Navbar = () => {
+    // const {
+    //     logout,
+    //   } = useAuth0();
+
+    //   const logoutWhenClickLanding = (page) => {
+    //     if(page === 'Landing Screen')
+    //         logout({ returnTo: window.location.origin });
+    //     else 
+    //         return
+    //   }
+
   return (
     <div className='bg-[#5893D4] shadow-lg'>
         <div className='container mx-auto'>
             <div className='min-h-[80px] flex justify-between items-center'>
                 {/* Logo & Company Name  */}
-                <div className='flex justify-start items-center space-x-2'>
-                    <div className='text-[40px] flex items-center text-[#1F3C88]'>
-                        <ion-icon name="thunderstorm-sharp"></ion-icon>
+                <div className='flex justify-start items-center space-x-10'>
+                    <div className='flex justify-start items-center space-x-1'>
+                        <div className='text-[40px] flex items-center text-[#1F3C88]'>
+                            <ion-icon name="thunderstorm-sharp"></ion-icon>
+                        </div>
+                        <div className='text-[20px] font-semibold'>
+                            <span className='text-white'>WEATHER</span>
+                            <span className='text-[#F7B633]'>FORECAST</span>
+                        </div>
                     </div>
-                    <div className='text-[20px] font-semibold'>
-                        <span className='text-white'>WEATHER</span>
-                        <span className='text-[#F7B633]'>FORECAST</span>
-                    </div>
+                    {/* <div>
+                        <ul className='flex justify-start items-center space-x-5'>
+                            {menu.map(items => {
+                                return(
+                                    <li key={items.id}  onClick={() => logoutWhenClickLanding(items.name)}>
+                                        <Link to={items.link}>{items.name}</Link>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </div> */}
                 </div>
                 {/* Logout button  */}
                 <div className='flex items-center'>
---
+++ src/constants/menu-const.js
@@ -0,0 +1,17 @@
+export const menu = [
+    {
+        id: 1,
+        name: 'Landing Screen',
+        link: '/login'
+    },
+    {
+        id: 2,
+        name: 'Home Screen',
+        link: '/'
+    },
+    {
+        id: 3,
+        name: 'Weather Screen',
+        link: '/weather'
+    },
+]
